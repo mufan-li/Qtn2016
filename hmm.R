6891dbe2e3d58ef897e3f1818d2bbcefa5d66e59
Mufan (Bill) Li <mufan.li@outlook.com>
2016-03-13 23:13:32 -0400

    Added posterior sampling for HMM

--- hmm.R
+++ hmm.R
@@ -1,18 +1,22 @@
 library(rstan)
 
+# Toy Example
 bull1 = rnorm( 100, 0.10, 0.15 )
 bear  = rnorm( 100, -0.01, 0.30 )
 bull2 = rnorm( 100, 0.10, 0.15 )
 z = c(rep(1,100),rep(2,100),rep(1,100))
-y = c( bull1, bear,  bull2 )
+y = c( bull1, bear,  bull2)
 
 alpha = c(0.3, 0.3)
 K = 2 # number of states
 N = 300
 
-iter = 200
+iter = 100
 chains = 1
+M = 50
+# init=list(list(mu=-10000,sigma=10), list(mu=20, sigma=0.1))
 hmm_fit <- stan(file="hmm.stan",
-                   data=c("K","N","y","alpha"),
+                   data=c("K","N","M","y","alpha"),
                    iter=iter,chains=chains)
-print(hmm_fit)
+# print(hmm_fit)
+hmm_fit_df <- as.data.frame(hmm_fit)

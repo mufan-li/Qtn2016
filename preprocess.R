16feccbf044a69a4d470f7d88883437767891e87
Mufan (Bill) Li <mufan.li@outlook.com>
2016-03-18 19:12:47 -0400

    Added preprocess and data with header

---
+++ preprocess.R
@@ -0,0 +1,15 @@
+
+in_data = read.csv("in_sample_data.txt",header=F)
+
+row = names(in_data)
+row[1] = "Date"
+for (i in 0:99) {
+	row[1 + i*6 + 1] = paste0("SO_",i)
+	row[1 + i*6 + 2] = paste0("SH_",i)
+	row[1 + i*6 + 3] = paste0("SL_",i)
+	row[1 + i*6 + 4] = paste0("SC_",i)
+	row[1 + i*6 + 5] = paste0("TVL_",i)
+	row[1 + i*6 + 6] = paste0("IND_",i)
+}
+names(in_data) = row
+write.csv(in_data,"in_sample_data_headers.csv")
